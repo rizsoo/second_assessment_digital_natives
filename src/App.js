4047c92909433d5b7d72707725dc61b5666946a8
rizsoo <89772889+rizsoo@users.noreply.github.com>
2022-02-27 18:50:16 +0100

    first steps

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { useState, useEffect } from 'react'
+
+function App() {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://assessment-users-backend.herokuapp.com/users")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+          console.log(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          <div key={item.id}>
+            <h3>{item.first_name} {item.last_name}</h3>
+          </div>
+        ))}
+      </ul>
+    );
+  }
+
+}
+
+export default App;
