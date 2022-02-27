78ea268ad724d5fa35c32c33cdc26bd616ef31ef
rizsoo <89772889+rizsoo@users.noreply.github.com>
2022-02-27 21:33:57 +0100

    1.exercise done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { useState, useEffect } from 'react'
+import ReactPaginate from 'react-paginate';
 
 function App() {
 
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [pageNum, setPageNum] = useState(0);
 
   useEffect(() => {
-    fetch("https://assessment-users-backend.herokuapp.com/users")
+    fetch("https://assessment-users-backend.herokuapp.com/users.json")
       .then(res => res.json())
       .then(
         (result) => {
@@ -23,19 +25,41 @@ function App() {
       )
   }, [])
 
+  const handlePageClick = (data) => {
+    setPageNum(data.selected);
+    console.log(data);
+  }
+
+  let nextNum = pageNum * 10;
+
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
   } else {
     return (
-      <ul>
-        {items.map(item => (
-          <div key={item.id}>
+      <div>
+        {items.filter((item, i) => i >= nextNum & i < nextNum + 10).map(item => (
+          <div className='profile' key={item.id}>
             <h3>{item.first_name} {item.last_name}</h3>
+            <p>Created at: {item.created_at}</p>
           </div>
         ))}
-      </ul>
+        <div className='pagination'>
+          <ReactPaginate 
+            onPageChange={handlePageClick}
+            containerClassName={'pagination justify-content-center'}
+            pageClassName={'page-item'}
+            pageCount={items.length / 10}
+            pageLinkClassName={'page-link'}
+            previousClassName={'page-item'}
+            previousLinkClassName={'page-link'}
+            nextClassName={'page-item'}
+            nextLinkClassName={'page-link'}
+            activeClassName={'active'}
+          />
+       </div>
+      </div>
     );
   }
 
